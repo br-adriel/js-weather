5cf3f4650bf3cc52ecadefb32d5bea5223670379
Adriel <adriel.fariasan@gmail.com>
2022-02-23 09:53:37 -0300

    Implementa nomes dos proximos dias dinamicamente

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const DATA_HOJE = new Date();
+
+// Carrega dias das proximas previsoes
+const semana = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const h4s = [...document.querySelectorAll(".grid-dias h4")];
+
+for (let i = 1; i < 3; i++) {
+  let dia = DATA_HOJE.getDay() + i + 1;
+  if (dia > 6) {
+    dia = dia - 7;
+  }
+  h4s[i].innerText = semana[dia];
+}
+
+// Visualização do form de localizacao
 const btnLocalizacao = document.getElementById("btnLocalizacao");
 const divForm = document.querySelector(".bloco-form");
 
